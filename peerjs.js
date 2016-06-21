3a004e3060cfd4361fea09d546a7dec762b25c9b
shrimats <srimathih@sahajsoft.com>
2016-06-21 19:54:15 +0530

    reverting adding audio strema

--- peerjs.js
+++ peerjs.js
@@ -70,7 +70,7 @@ function step1() {
     //   peerConnection.addStream(mixedOutput.stream);
 		$('#my-video').prop('src', URL.createObjectURL(stream));
 
-		window.localStream.addStream(mixedOutput.stream);
+		window.localStream.addStream(stream);
 		step2();
 	}, function(){ $('#step1-error').show(); });
 }
